e893da7708a3911a21180527688363e83925a366
yosiarbilla <yosiarbl16@gmail.com>
2024-10-29 15:59:56 +0700

    login page baru

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
