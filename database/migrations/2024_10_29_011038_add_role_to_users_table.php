2d6b031e157f5808c2ad20dff0c54fd65a9c0f2c
Drasticia <132827705+Drasticia@users.noreply.github.com>
2024-11-07 13:01:50 +0700

    Role update database

--- database/migrations/2024_10_29_011038_add_role_to_users_table.php
+++ database/migrations/2024_10_29_011038_add_role_to_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->enum('role', ['dokter', 'administrasi'])->after('password');
+            // Menggunakan enum untuk membatasi nilai hanya "dokter" atau "administrasi".
         });
     }
 
@@ -22,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 };
